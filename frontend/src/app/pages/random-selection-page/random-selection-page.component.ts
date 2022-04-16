0757d6344bf41a29ade5826c60e7cba051a97135
Sebastian Lysek <sebastian.lysek@gmx.de>
2022-04-16 14:24:44 +0200

    gitignore and clean code fixes

--- frontend/src/app/pages/random-selection-page/random-selection-page.component.ts
+++ frontend/src/app/pages/random-selection-page/random-selection-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {MovieService} from '../../services/movie.service';
 import {Movie} from '../../models/Movie';
 import {MatTableDataSource} from '@angular/material/table';
@@ -10,19 +10,19 @@ import {ClipboardModule, ClipboardService} from 'ngx-clipboard';
   templateUrl: './random-selection-page.component.html',
   styleUrls: ['./random-selection-page.component.css']
 })
-export class RandomSelectionPageComponent implements OnInit {
+export class RandomSelectionPageComponent implements OnInit, AfterViewInit {
   movieservice: MovieService;
   movies: Movie[] = [];
   watched: boolean[] = [false, false, false];
 
-  constructor(http:HttpClient, private _clipboardService: ClipboardService) {
+  // tslint:disable-next-line:variable-name
+  constructor(http: HttpClient, private _clipboardService: ClipboardService) {
     this.movieservice = new MovieService(http);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async ngAfterViewInit() {
+  async ngAfterViewInit(): Promise<any> {
     // tslint:disable-next-line:max-line-length
     await this.movieservice.getRandomMovies().toPromise().then(res => res.body).then(movies => movies.forEach(movie => this.movies.push(new Movie(movie.id, movie.title, movie.year, movie.runtime, movie.director, movie.rating, movie.imdbLink, movie.posterLink, movie.watched, movie.personalRating, movie.votes, movie.genre, movie.plot, movie.actors))));
   }
